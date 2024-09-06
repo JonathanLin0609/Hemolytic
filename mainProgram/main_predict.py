47a1133d33a410142fe26be09c31ed488779d75c
JonathanLin0609 <145912636+JonathanLin0609@users.noreply.github.com>
2024-09-07 00:10:46 +0800

    Add files via upload

---
+++ mainProgram/main_predict.py
@@ -0,0 +1,25 @@
+from userPackage.Package_IHEPC import IHEPC_Predict
+
+# If you want to use a different model, you can change the dataset
+dataset = 'DS2'
+if dataset == 'DS1':
+    model_use = '1'
+elif dataset == 'DS2':
+    model_use = '2'
+
+
+# Path setting
+pathDict = {'paramPath': f'../data/param/{dataset}/',  # This path should have featureTypeDict.pkl and robust.pkl
+            'saveCsvPath': '../data/mlData/new_data/',  # Your encoded data will save in this path
+            'modelPath': f'../data/finalModel/{dataset}/',  # This path should have 'rbfsvm', 'lightgbm', 'gbc' models. ex: gbc_final.pkl
+            'outputPath': f'../data/output/{dataset}/'}  # Your prediction will save in this path
+
+# Input your FASTA file, the example file can find in data/mlData/DS1/test_neg.FASTA
+inputPathList = [f'../data/mlData/{dataset}/test_neg.FASTA', f'../data/mlData/{dataset}/test_pos.FASTA']
+
+encapObj = IHEPC_Predict(model_use=model_use, pathDict=pathDict)
+encapObj.loadData(inputDataList=inputPathList)
+encapObj.featureEncode()
+encapObj.doPredict()
+
+
